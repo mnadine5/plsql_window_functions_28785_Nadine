7f7cad4a3a5c079d64baee6792ce5c4919f76f56
mnadine5 <nadinemurora5@gmail.com>
2026-02-01 15:56:12 +0200

    Joins.sql

---
+++ Joins.sql
@@ -0,0 +1,9 @@
+1. INNER Join : Retrieve trasactions with valid customers and products.
+   ## Only returns transactions where both customer and products exists.
+2. LEFT JOIN — Identify customers who have never made a transaction.
+   ##
+3.RIGHT JOIN (or FULL JOIN) — Detect products with no sales activity.
+   ##
+4.FULL OUTER JOIN — Compare customers and products including unmatched records.
+  ## Combines all customers, all transactions and all products even is some are unmatched.
+5.SELF JOIN — Compare customers within the same region.
