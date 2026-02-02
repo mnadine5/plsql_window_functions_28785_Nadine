c3f1c6ee45f25de30368df114be667bdaff4e3e7
mnadine5 <nadinemurora5@gmail.com>
2026-02-02 14:50:10 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -20,21 +20,21 @@ CREATE TABLE Sales (
     total_amount NUMBER(10, 2)
 );
 
-INSERT INTO Customers (name, location) VALUES ('John Doe', 'Kigali');
-INSERT INTO Customers (name, location) VALUES ('Jane Smith', 'Kigali');
-INSERT INTO Customers (name, location) VALUES ('Alice Johnson', 'Musanze');
-INSERT INTO Customers (name, location) VALUES ('Bob Brown', 'Musanze');
-INSERT INTO Customers (name, location) VALUES ('Charlie Davis', 'Huye');
+INSERT INTO Customers (customer_id,name, location) VALUES (1,'John Doe', 'Kigali');
+INSERT INTO Customers (customer_id,name, location) VALUES (2,'Jane Smith', 'Kigali');
+INSERT INTO Customers (customer_id,name, location) VALUES (3,'Alice Johnson', 'Musanze');
+INSERT INTO Customers (customer_id,name, location) VALUES (4,'Bob Brown', 'Musanze');
+INSERT INTO Customers (customer_id,name, location) VALUES (5,'Charlie Davis', 'Huye');
 
-INSERT INTO Medicines (name, category, price) VALUES ('Paracetamol', 'Painkillers', 5.00);
-INSERT INTO Medicines (name, category, price) VALUES ('Amoxicillin', 'Antibiotics', 15.00);
-INSERT INTO Medicines (name, category, price) VALUES ('Ibuprofen', 'Painkillers', 8.00);
-INSERT INTO Medicines (name, category, price) VALUES ('Aspirin', 'Painkillers', 4.00);
-INSERT INTO Medicines (name, category, price) VALUES ('Insulin', 'Diabetes', 50.00);
+INSERT INTO Medicines (medicine_id ,name, category, price) VALUES (1,'Paracetamol', 'Painkillers', 5.00);
+INSERT INTO Medicines (medicine_id ,name, category, price) VALUES (2,'Amoxicillin', 'Antibiotics', 15.00);
+INSERT INTO Medicines (medicine_id ,name, category, price) VALUES (3,'Ibuprofen', 'Painkillers', 8.00);
+INSERT INTO Medicines (medicine_id ,name, category, price) VALUES (4,'Aspirin', 'Painkillers', 4.00);
+INSERT INTO Medicines (medicine_id ,name, category, price) VALUES (5,'Insulin', 'Diabetes', 50.00);
 
 
-INSERT INTO Sales (customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (1, 1, TO_DATE('2025-01-15', 'YYYY-MM-DD'), 2, 10.00);
-INSERT INTO Sales (customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (2, 2, TO_DATE('2025-01-20', 'YYYY-MM-DD'), 1, 15.00);
-INSERT INTO Sales (customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (3, 3, TO_DATE('2025-02-10', 'YYYY-MM-DD'), 3, 24.00);
-INSERT INTO Sales (customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (4, 1, TO_DATE('2025-02-15', 'YYYY-MM-DD'), 1, 5.00);
-INSERT INTO Sales (customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (5, 4, TO_DATE('2025-03-05', 'YYYY-MM-DD'), 2, 20.00);
+INSERT INTO Sales (sale_id ,customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (1,1, 1, TO_DATE('2025-01-15', 'YYYY-MM-DD'), 2, 10.00);
+INSERT INTO Sales (sale_id ,customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (2,2, 2, TO_DATE('2025-01-20', 'YYYY-MM-DD'), 1, 15.00);
+INSERT INTO Sales (sale_id ,customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (3,3, 3, TO_DATE('2025-02-10', 'YYYY-MM-DD'), 3, 24.00);
+INSERT INTO Sales (sale_id ,customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (4,4, 1, TO_DATE('2025-02-15', 'YYYY-MM-DD'), 1, 5.00);
+INSERT INTO Sales (sale_id ,customer_id, medicine_id, sale_date, quantity, total_amount) VALUES (5,5, 4, TO_DATE('2025-03-05', 'YYYY-MM-DD'), 2, 20.00);
